97390cf861d476bffd312adcd30b169514ce720d
kartik <kartik.kumar53@gmail.com>
2024-01-30 15:04:47 +0530

    Add project files.

---
+++ BlazorAppSacfolding/Data/BlazorAppSacfoldingContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlazorAppSacfolding;
+
+namespace BlazorAppSacfolding.Data
+{
+    public class BlazorAppSacfoldingContext : DbContext
+    {
+        public BlazorAppSacfoldingContext (DbContextOptions<BlazorAppSacfoldingContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BlazorAppSacfolding.Blog> Blog { get; set; } = default!;
+    }
+}
